4fb255c23dabfed6dad097133c3e187b3d0f977c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-10-01 15:45:34 -0300

    Refactored to adapt to Python best practices

--- csv2b64img.py
+++ csv2b64img.py
@@ -4,6 +4,7 @@
 #region import
 import sys
 import base64
+from typing import NamedTuple
 from wand.image import Image
 from wand.drawing import Drawing
 from wand.color import Color
@@ -22,6 +23,43 @@ CORES = [
   'rgb(128,255,128)'
 ]
 
+class ImageCannotBeGenerateError(BaseException):
+  """ Exeption that oocurs when image cannot be generated """
+
+class Propriedades(NamedTuple):
+  """ class to hold some values """
+  destacar: bool
+  quantidades: int
+  tamanhos: list[int]
+  caracteres: int
+  largura: int
+  altura: int
+
+def get_greatest_elements(vec1, vec2) -> list[int]:
+  """ Function to get the greatest elements """
+  if len(vec1) != len(vec2):
+    raise ValueError("Vectors must be of the same size.")
+  # Use a list comprehension with the built-in max() function
+  return [max(v1, v2) for v1, v2 in zip(vec1, vec2)]
+
+def get_csv_definitions(csv_data: str) -> Propriedades:
+  """ Method to get Propriedades of csv data """
+  if len(csv_data.strip()) == 0:
+    raise ValueError("Invalid CSV input.")
+  propriedades = Propriedades()
+  propriedades.destacar = csv_data.startswith('#')
+  linhas = csv_data.split(SEPARADOR_ENTRE_LINHAS)
+  propriedades.quantidades = sum(len(x.strip()) > 0 for x in linhas)
+  propriedades.tamanhos = [0 for x in linhas[0].split(SEPARADOR_ENTRE_COLUNAS)]
+  for linha in linhas:
+    colunas = linha.strip().split(SEPARADOR_ENTRE_COLUNAS)
+    tamanhos_colunas = [len(x) for x in colunas]
+    propriedades.tamanhos = get_greatest_elements(tamanhos_colunas, propriedades.tamanhos)
+  propriedades.caracteres = sum(propriedades.tamanhos)
+  propriedades.largura = propriedades.caracteres * LARGURA_CARACTERE
+  propriedades.altura = propriedades.quantidades * ALTURA_CARACTERE
+  return propriedades
+
 def try_parse_int(arg: str):
   """ Get int value from str """
   try:
@@ -31,31 +69,15 @@ def try_parse_int(arg: str):
 
 def generate_image_from_csv(csv_data: str) -> str:
   """Generate base64 image string from CSV-like input"""
+  propriedades = get_csv_definitions(csv_data)
   linhas = csv_data.split(SEPARADOR_ENTRE_LINHAS)
   if not linhas or len(linhas[0].strip()) == 0:
     raise ValueError("Invalid CSV input.")
-  tamanho_colunas_relatorio = [0] * len(linhas[0].split(SEPARADOR_ENTRE_COLUNAS))
-  destacar_linhas = linhas[0].startswith('#')
-  quantidade_linhas_relatorio = 0
-  for i, linha in enumerate(linhas):
-    if not linha:
-      continue
-    quantidade_linhas_relatorio += 1
-    colunas = linha.split(SEPARADOR_ENTRE_COLUNAS)
-    for j, coluna in enumerate(colunas):
-      if destacar_linhas and j == 0:
-        continue
-      if len(coluna) >= tamanho_colunas_relatorio[j]:
-        tamanho_colunas_relatorio[j] = len(coluna) + 1
-  # tamanho_colunas_relatorio = [x + 2 for x in tamanho_colunas_relatorio]
-  caracteres_total = sum(tamanho_colunas_relatorio)
-  largura_total_imagem = caracteres_total * LARGURA_CARACTERE
-  altura_total_imagem = quantidade_linhas_relatorio * ALTURA_CARACTERE
   # Generate the image
   with Drawing() as draw:
     with Image(
-      width = largura_total_imagem,
-      height = altura_total_imagem,
+      width = propriedades.largura,
+      height = propriedades.altura,
       background = Color(CORES[0])
       ) as img:
       draw.font_family = 'Consolas'
@@ -67,19 +89,17 @@ def generate_image_from_csv(csv_data: str) -> str:
           continue
         colunas = linha.split(SEPARADOR_ENTRE_COLUNAS)
         for j, coluna in enumerate(colunas):
-          if destacar_linhas and j == 0:
+          if propriedades.destacar and j == 0:
             if i == 0:
               continue
             cor = try_parse_int(coluna)
             if cor:
-              top_margin = (i * ALTURA_CARACTERE) + 1
-              botton_margin = (i * ALTURA_CARACTERE) + ALTURA_CARACTERE + 1
               draw.fill_color = Color(CORES[cor])
               draw.rectangle(
                 left = 0,
-                right = largura_total_imagem,
-                top = top_margin,
-                bottom = botton_margin)
+                right = propriedades.largura,
+                top = (i * ALTURA_CARACTERE) + 1,
+                bottom = (i * ALTURA_CARACTERE) + ALTURA_CARACTERE + 1)
               draw.fill_color = Color(CORES[1])
           else:
             if coluna:
@@ -89,11 +109,11 @@ def generate_image_from_csv(csv_data: str) -> str:
                   y = ((i + 1) * ALTURA_CARACTERE),
                   body = letra)
                 cursor += LARGURA_CARACTERE
-          cursor = sum(tamanho_colunas_relatorio[:j + 1]) * LARGURA_CARACTERE
+          cursor = sum(propriedades.tamanhos[:j + 1]) * LARGURA_CARACTERE
       draw(img)
       img_bytes = img.make_blob(format='png')
       if not img_bytes:
-        raise Exception()
+        raise ImageCannotBeGenerateError()
       img_base64 = base64.b64encode(img_bytes).decode('utf-8')
       return img_base64
 
@@ -103,8 +123,10 @@ def main():
   try:
     img_base64 = generate_image_from_csv(valores)
     print(img_base64)
-  except:
+  except ImageCannotBeGenerateError:
     print('500: Image cannot be generated')
+  except ValueError:
+    print('400: Input is not a valid CSV data')
 
 if __name__ == '__main__':
   main()
